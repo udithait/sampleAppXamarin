29456800c9c4e8ead9324b2fa2ff626ea394c2ab
Uditha <Uditha@LAPTOP-06OH8UR6>
2022-05-13 14:52:53 +0800

    Add project files.

---
+++ Droid/SplashActivity.cs
@@ -0,0 +1,15 @@
+﻿using Android.App;
+using Android.OS;
+
+namespace sampleAppXamarin.Droid
+{
+    [Activity(Theme = "@style/Theme.Splash", MainLauncher = true, NoHistory = true)]
+    public class SplashActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            this.StartActivity(typeof(MainActivity));
+        }
+    }
+}
---
+++ sampleAppXamarin/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using Xamarin.Forms;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+
+namespace sampleAppXamarin
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            AppCenter.Start("android={207fa13f-7e32-416d-ba75-b087bc7cf3e4};" +
+                   "ios={83f0590b-0593-4dbf-9a7f-69b74de8c3bd};",
+                   typeof(Analytics), typeof(Crashes));
+            MainPage = new ButtonPage();
+        }
+    }
+}
